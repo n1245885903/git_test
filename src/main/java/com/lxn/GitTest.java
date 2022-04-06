2e43bac613e13f6fd43adc15125240b91d8b2815
李鑫宁 <10699261+n1245885903@user.noreply.gitee.com>
2022-04-06 17:58:51 +0800

    first commit

---
+++ src/main/java/com/lxn/GitTest.java
@@ -0,0 +1,11 @@
+package com.lxn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+
+    }
+}
