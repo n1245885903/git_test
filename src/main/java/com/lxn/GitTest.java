069d85dcf08f082839875a7330260ec9ca90345a
李鑫宁 <10699261+n1245885903@user.noreply.gitee.com>
2022-04-06 18:01:38 +0800

    second commit

--- src/main/java/com/lxn/GitTest.java
+++ src/main/java/com/lxn/GitTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git2!!");
 
     }
 }
